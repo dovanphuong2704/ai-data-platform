09ba5954d09f2954150fa55b163a01eb3573abd7
Đỗ Phương <dophuong27423@gmail.com>
2026-03-31 16:14:01 +0700

    feat: configure basePath and assetPrefix for next-intl integration in next.config.ts

--- client/next.config.ts
+++ client/next.config.ts
@@ -4,6 +4,10 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./src/i18n.ts");
 
 const nextConfig: NextConfig = {
+  /* Thêm 2 dòng này */
+  basePath: '/chatdb',
+  assetPrefix: '/chatdb',
+
   /* config options here */
 };
 
